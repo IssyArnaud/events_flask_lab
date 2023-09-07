80c373ecbbe292787a7412bef88b258c3cb1e774
Issy Arnaud <yarnaud36@gmail.com>
2023-09-07 09:29:10 +0100

    for homework

--- models/events_list.py
+++ models/events_list.py
@@ -1,8 +1,9 @@
 from models.event import Event
+import datetime
 
-event_01 = Event("05/08/2023", "Late Night Comedy", 26, "The Stand", "hilarious late night comedy", True)
-event_02 = Event("12/08/2023", "Magic Show", 48, "Underbelly", "magical fun for all the family", False)
-event_03 = Event("20/08/2023", "The Lady Boys of Bangkok", 135, "Usher Hall", "sparkly dance show", True)
+event_01 = Event(datetime.date(2023, 8, 5), "Late Night Comedy", 26, "The Stand", "hilarious late night comedy", True)
+event_02 = Event(datetime.date(2023, 8, 12), "Magic Show", 48, "Underbelly", "magical fun for all the family", False)
+event_03 = Event(datetime.date(2023, 8, 20), "The Lady Boys of Bangkok", 135, "Usher Hall", "sparkly dance show", True)
 
 events = [event_01, event_02, event_03]
 
